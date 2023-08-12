499385a1781cfcb5d882e4e0b7de8c062dd0f8cf
roastedelectrons <114177722+roastedelectrons@users.noreply.github.com>
2023-08-12 11:36:37 +0200

    Initial commit

---
+++ DynamicListForm/module.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+	class DynamicListForm extends IPSModule
+	{
+		public function Create()
+		{
+			//Never delete this line!
+			parent::Create();
+
+			$this->RegisterPropertyString('List', '');
+		}
+
+		public function Destroy()
+		{
+			//Never delete this line!
+			parent::Destroy();
+		}
+
+		public function ApplyChanges()
+		{
+			//Never delete this line!
+			parent::ApplyChanges();
+		}
+
+		public function UpdateForm( $name, $value)
+		{
+			switch ($name)
+			{
+				case 'Type':
+					if ($value == 0)
+					{
+						$this->UpdateFormField('VariableID', 'visible', true);
+						$this->UpdateFormField('Value', 'visible', false);
+					} else {
+						$this->UpdateFormField('VariableID', 'visible', false);
+						$this->UpdateFormField('Value', 'visible', true);
+					}
+
+					break;
+				default:
+					print_r('Ergebnis: '.$action.' Value: '.$value);
+			}
+		}
+	}
---
+++ README.md
@@ -0,0 +1,6 @@
+# ConfigurationFormTest
+
+Folgende Module beinhaltet das DynamicListForm Repository:
+
+- __DynamicListForm__ ([Dokumentation](DynamicListForm))  
+	Kurze Beschreibung des Moduls.
